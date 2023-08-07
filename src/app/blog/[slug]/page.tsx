a3f526813accd16156a787e687dfab7186cac695
Stefan Logue <stefanlogue@icloud.com>
2023-08-07 18:18:24 +0100

    feat: Initial commit

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page = ({ params }: { params: { slug: string } }) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export const generateStaticParams = async () => {
+
+}
+
+export default Page
