3bbaef92ff1ff99ac05665c195e00f54f5f0190e
Stefan Logue <stefanlogue@icloud.com>
2023-08-08 20:38:44 +0100

    fix: hopeefully it will deploy

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -25,20 +25,18 @@ const DarkModeToggle = (): JSX.Element => {
     setDarkMode(isDark())
   }, [])
 
-  const darkModeActive: boolean =
-    typeof window && document.documentElement.classList.contains('dark')
   return (
     <AnimatePresence mode='wait' initial={false}>
       <motion.button
         className="text-2xl sm:text-3xl text-yellow-400 dark:text-yellow-300 focus:outline-none"
         onClick={() => toggleMode()}
-        key={darkModeActive ? 'dark-icon' : 'light-icon'}
+        key={isDarkMode ? 'dark-icon' : 'light-icon'}
         initial={{ y: -20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         exit={{ y: 20, opacity: 0 }}
         transition={{ duration: 0.2 }}
       >
-        {darkModeActive ? '🌙' : '🌤️'}
+        {isDarkMode ? '🌙' : '🌤️'}
       </motion.button>
     </AnimatePresence>
   )
